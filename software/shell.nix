54af50de6e2c1fe92591d401437edc71abfc4747
MiikaMatias <miikapiiparinen24@gmail.com>
2025-02-04 23:04:19 -0500

    flakes rebuild

--- software/shell.nix
+++ software/shell.nix
@@ -6,7 +6,7 @@
       autosuggestions.enable=true;
       shellAliases = {
         config="code /etc/nixos";
-        update="git add /etc/nixos/. && git commit -m \"flakes rebuild $(date '+%Y-%m-%d %H:%M:%S')\" && git push && sudo nixos-rebuild switch --flake . --impure"
+        update="git add /etc/nixos/. && git commit -m \"flakes rebuild $(date '+%Y-%m-%d %H:%M:%S')\" && git push && sudo nixos-rebuild switch --flake . --impure";
         f="firefox";
         gs="git status";
         ga="git add";
