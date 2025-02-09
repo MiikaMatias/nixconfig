ae5abc87ba8a75bccb8092695d694b8619083c88
MiikaMatias <miikapiiparinen24@gmail.com>
2025-02-08 23:00:55 -0500

    flakes rebuild 2025-02-08 23:00:55  Changes: development/nginx.nix

--- development/nginx.nix
+++ development/nginx.nix
@@ -1,16 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  services.nginx = {
-    enable = true;
-    virtualHosts."kontrakti.com" = {
-      forceSSL = false;
-      enableACME = false;
-      locations."/.well-known/acme-challenge/" = {
-        root = "/var/lib/acme";
+    services.nginx = {
+      enable = true;
+      virtualHosts."kontrakti.com" = {
+        listen = [
+          { addr = "0.0.0.0"; port = 8080; ssl = false; }  # Listen on port 8080
+        ];
+        root = "/home/miika/Projects/portfolio/templates";
+        locations."/" = {  
+          extraConfig = ''
+            index index.html;
+          '';
+        };
       };
     };
-  };
   
   /*
   security.acme = {
