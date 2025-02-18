7ee7f5efa56444d7cac4b80ec850f9b4194b5665
MiikaMatias <miikapiiparinen24@gmail.com>
2025-02-18 17:43:19 -0500

    flakes rebuild 2025-02-18 17:43:19  Changes: system/windowmanager.nix

--- system/windowmanager.nix
+++ system/windowmanager.nix
@@ -8,7 +8,6 @@
     services.xserver.windowManager.dwm.enable = true;
     services.xserver.displayManager.defaultSession = "none+dwm";
 
-
     /*
     services.xserver.windowManager.dwm.package = pkgs.dwm.override {
         patches = [
