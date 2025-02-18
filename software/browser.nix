ebd1f0cf79743521762efa0a64b6c13737c2f80b
MiikaMatias <miikapiiparinen24@gmail.com>
2025-02-18 17:40:32 -0500

    fuck

--- software/browser.nix
+++ software/browser.nix
@@ -4,11 +4,7 @@
       enable = true;
   };
 
-<<<<<<< Updated upstream
-  environment.systemPackages = with pkgs ; [ 
-=======
   environment.systemPackages = with pkgs; [
->>>>>>> Stashed changes
       w3m
   ];
 }
