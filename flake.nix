5e5b46f77f059f4c50b4b18c7f4f9d57553cb693
MiikaMatias <miikapiiparinen24@gmail.com>
2025-02-25 20:05:16 -0500

    flakes rebuild 2025-02-25 20:05:16  Changes: flake.lock flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = { self, nixpkgs}: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
