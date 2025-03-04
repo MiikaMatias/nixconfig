4c3e63c9c746d4def2b3305a8746109ed6d3982c
MiikaMatias <miikapiiparinen24@gmail.com>
2025-03-04 04:14:57 -0500

    flakes rebuild 2025-03-04 04:14:57  Changes: software/terminal.nix

--- software/terminal.nix
+++ software/terminal.nix
@@ -4,10 +4,10 @@
     environment.systemPackages = with pkgs; [
       (st.overrideAttrs (oldAttrs: rec {
           src = fetchFromGitHub {
-            owner = "LukeSmithxyz";
+            owner = "MiikaMatias";
             repo = "st";
-            rev = "8ab3d03681479263a11b05f7f1b53157f61e8c3b";
-            sha256 = "1brwnyi1hr56840cdx0qw2y19hpr0haw4la9n0rqdn0r2chl8vag";
+            rev = "c79221655e406a163beab9fff8ab9f72ac685638";
+            sha256 = "sha256-LH4Mvgn39cvlLBmLKnUijnOyycz4QNsRo5z5cxSrEHo=";
           };
           buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
         }))
