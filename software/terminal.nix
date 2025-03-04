9710884365c2615bbc34c9ff7a8e3e0c4c6aa52e
MiikaMatias <miikapiiparinen24@gmail.com>
2025-03-04 03:05:27 -0500

    flakes rebuild 2025-03-04 03:05:27  Changes: software/terminal.nix suckless/dwm

--- software/terminal.nix
+++ software/terminal.nix
@@ -1,4 +1,13 @@
 {config, pkgs, ...} :
 {
-    pkgs.st.override { conf = ./../suckless/st/config.h; };
+    /*
+    nixpkgs.overlays = [
+        (self: super: {
+        dwm = super.dwm.overrideAttrs (oldAttrs: rec {
+        configFile = super.writeText "config.h" (builtins.readFile ./dwm-config.h);
+        postPatch = oldAttrs.postPatch or "" + "\necho 'Using own config file...'\n cp ${configFile} config.def.h";
+        });
+        })
+    ];
+    */
 }
