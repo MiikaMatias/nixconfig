21475f2e347e100a281294c77f5b5ea2eb1100e7
MiikaMatias <miikapiiparinen24@gmail.com>
2025-03-04 04:01:28 -0500

    flakes rebuild 2025-03-04 04:01:28  Changes: suckless/st system/displayserver.nix

--- system/displayserver.nix
+++ system/displayserver.nix
@@ -3,7 +3,4 @@
     services.xserver.enable=true;
     services.xserver.videoDrivers=["nvidia"];
     services.xserver.windowManager.dwm.enable = true;
-
-    # to test if st patch works
-    services.picom.enable = true;
 }
