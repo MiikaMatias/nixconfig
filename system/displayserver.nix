583c21b9b29d72bcc50637dec73a4ca4c1498907
MiikaMatias <miikapiiparinen24@gmail.com>
2025-03-04 03:58:59 -0500

    flakes rebuild 2025-03-04 03:58:59  Changes: system/displayserver.nix

--- system/displayserver.nix
+++ system/displayserver.nix
@@ -3,4 +3,7 @@
     services.xserver.enable=true;
     services.xserver.videoDrivers=["nvidia"];
     services.xserver.windowManager.dwm.enable = true;
+
+    # to test if st patch works
+    services.picom.enable = true;
 }
