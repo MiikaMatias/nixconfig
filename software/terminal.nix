0a39df8e6472759a934c3ffab60290c781659cce
MiikaMatias <miikapiiparinen24@gmail.com>
2025-03-04 03:11:56 -0500

    flakes rebuild 2025-03-04 03:11:56  Changes: software/terminal.nix

--- software/terminal.nix
+++ software/terminal.nix
@@ -1,13 +1,17 @@
 {config, pkgs, ...} :
 {
-    /*
+    environment.systemPackages = with pkgs; [
+        st
+    ];
+    
     nixpkgs.overlays = [
         (self: super: {
-        dwm = super.dwm.overrideAttrs (oldAttrs: rec {
-        configFile = super.writeText "config.h" (builtins.readFile ./dwm-config.h);
-        postPatch = oldAttrs.postPatch or "" + "\necho 'Using own config file...'\n cp ${configFile} config.def.h";
+        st = super.st.overrideAttrs (oldAttrs: rec {
+            src = fetchGit {
+                url = "https://github.com/MiikaMatias/st.git";
+            }; 
         });
         })
     ];
-    */
+    
 }
