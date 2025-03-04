fb9bb843d15b75da4238b1d0acdd24959d32aed9
MiikaMatias <miikapiiparinen24@gmail.com>
2025-03-04 04:18:38 -0500

    flakes rebuild 2025-03-04 04:18:38  Changes: software/terminal.nix suckless/st

--- software/terminal.nix
+++ software/terminal.nix
@@ -6,7 +6,7 @@
           src = fetchFromGitHub {
             owner = "MiikaMatias";
             repo = "st";
-            rev = "c79221655e406a163beab9fff8ab9f72ac685638";
+            rev = "3a621013821bca772ce16e4dde35b4da29d13f5f";
             sha256 = "sha256-LH4Mvgn39cvlLBmLKnUijnOyycz4QNsRo5z5cxSrEHo=";
           };
           buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
