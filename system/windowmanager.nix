08e694dee1fb64b54013db68ade00bc0a4ace341
MiikaMatias <miikapiiparinen24@gmail.com>
2025-03-07 12:43:44 -0500

    flakes rebuild 2025-03-07 12:43:44  Changes: suckless/dwm system/windowmanager.nix

--- system/windowmanager.nix
+++ system/windowmanager.nix
@@ -12,7 +12,7 @@
             dwm = super.dwm.overrideAttrs (oldattrs: {
                 src = fetchGit {
                     url = "https://github.com/MiikaMatias/dwm.git";
-                    rev = "3f9bcb78e7bae6db96897b81ca5e18d64f861374";
+                    rev = "e29b402a908c526062da346c9d52712adb2322cb";
                 }; 
             });
         })
