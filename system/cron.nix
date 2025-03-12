87bac6c3cebc1c313eb1ec8a869f018675ef61f3
MiikaMatias <miikapiiparinen24@gmail.com>
2025-03-12 01:51:08 -0400

    flakes rebuild 2025-03-12 01:51:08  Changes: system/cron.nix

--- system/cron.nix
+++ system/cron.nix
@@ -3,8 +3,8 @@
   systemd.timers."dwm-bar" = {
     wantedBy = [ "timers.target" ];
       timerConfig = {
-        OnBootSec = "1m";
-        OnUnitActiveSec = "1m";
+        OnBootSec = "1s";
+        OnUnitActiveSec = "1s";
         Unit = "dwm-bar.service";
         persistent = true;
       };
