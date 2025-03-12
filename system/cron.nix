80f0cd089ed5925f84a14bbab842e3ce3bffd1d4
MiikaMatias <miikapiiparinen24@gmail.com>
2025-03-12 01:04:06 -0400

    flakes rebuild 2025-03-12 01:04:06  Changes: system/cron.nix

--- system/cron.nix
+++ system/cron.nix
@@ -4,7 +4,7 @@
     enable = true;
     systemCronJobs = [
       "*/5 * * * *      root    date >> /tmp/cron.log"
-      "* * * * *        root    update-dwm-bar"
+      "* * * * *        miika    update-dwm-bar"
     ];
   };
 }   
