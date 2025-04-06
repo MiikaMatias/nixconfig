81f8050cd7bc3d818992abd4a4d1f35b6567e746
MiikaMatias <miikapiiparinen24@gmail.com>
2025-04-06 18:05:58 -0400

    lol

--- development/gamedev.nix
+++ development/gamedev.nix
@@ -3,7 +3,11 @@
     environment.systemPackages = [
         pkgs.godot_4
         pkgs.jdk17
+        pkgs.android-studio
     ];
 
-    # Installing android here
+    # Installing android here since using it for gamedev
+    nixpkgs.config = {
+      android_sdk.accept_license = true;
+    };
 }
