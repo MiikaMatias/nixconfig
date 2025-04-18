19ba2a05cba407bbb2a65921a250c277aac56b69
MiikaMatias <miikapiiparinen24@gmail.com>
2025-04-18 18:15:13 -0400

    flakes rebuild 2025-04-18 18:15:13  Changes: development/gamedev.nix

--- development/gamedev.nix
+++ development/gamedev.nix
@@ -1,13 +1,15 @@
 {config, lib, pkgs, ...}:
 {
+    /*
     environment.systemPackages = [
         pkgs.godot_4
         pkgs.jdk17
-        pkgs.android-studio
+        pkgs.android-studio-full
     ];
 
     # Installing android here since using it for gamedev
     nixpkgs.config = {
       android_sdk.accept_license = true;
     };
+    */
 }
